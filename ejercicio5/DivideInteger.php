7ca8313529a5cc5037c03023f921b6a8e82fa5f5
JavierDiazDaw <javier.diaz.roman.al@iespoligonosur.org>
2022-02-13 21:24:21 +0100

    ejercicio 5 y 6

---
+++ ejercicio5/DivideInteger.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Calcular Letra DNI - Servicio Web + PHP + SOAP</title>
+    <link rel="stylesheet" type="text/css" href="/estilo.css">
+</head>
+
+<body>
+    <?php
+    $error = "";
+    $resultado = "";
+    $dni = "";
+
+    // Iniciamos el cliente SOAP
+    // Escribimos la dirección donde se encuentra el servicio
+    $wsdl = 'https://www.crcind.com/csp/samples/SOAP.Demo.CLS?WSDL'; //URL de nuestro servicio soap
+
+
+    // Ejecutamos las siguientes líneas al enviar el formulario
+    if (isset($_POST['enviar'])) {
+
+        $params = array(
+            "Arg1" => $_POST['num1'],
+            "Arg2" => $_POST['num2']
+        );
+
+        $options = array(
+            "uri" => $wsdl,
+            "style" => SOAP_RPC,
+            "use" => SOAP_ENCODED,
+            "soap_version" => SOAP_1_1,
+            "cache_wsdl" => WSDL_CACHE_BOTH,
+            "connection_timeout" => 15,
+            "trace" => false,
+            "encoding" => "UTF-8",
+            "exceptions" => false,
+        );
+
+        $cliente = new SoapClient($wsdl, $options);
+
+        if (!empty($_POST['num1'])) {
+
+            $resultado = $cliente->DivideInteger($params)->DivideIntegerResult;
+        } else {
+            $error = "<strong>Error:</strong> Debes introducir dos numeros<br/><br/>";
+        }
+    }
+    ?>
+    <h1>División</h1>
+    <h2>Servicio Web + PHP + SOAP</h2>
+    <form action="DivideInteger.php" method="post">
+        <?php
+        print "<input type='number' name='num1'>";
+        print "<input type='number' name='num2'>";
+        print "<input type='submit' name='enviar' value='Calcular'>";
+        print "<p class='error'>$error</p>";
+        print "<p style='font-size: 12pt;font-weight: bold;color: #0066CC;'>$resultado</p>";
+        ?>
+    </form>
+</body>
+
+</html>
---
+++ ejercicio5/FindPerson.php
@@ -0,0 +1,97 @@
+<?php
+// Vaciamos algunas variables
+$error = "";
+$resultado = "";
+$dni = "";
+
+
+$wsdl = 'https://www.crcind.com/csp/samples/SOAP.Demo.CLS?WSDL'; //URL de nuestro servicio soap
+
+//Basados en la estructura del servicio armamos un array
+
+
+//Enviamos el Request
+
+
+if (isset($_POST['enviar'])) {
+    $params = array(
+        "id" => $_POST['id'],
+    );
+
+    $options = array(
+        "uri" => $wsdl,
+        "style" => SOAP_RPC,
+        "use" => SOAP_ENCODED,
+        "soap_version" => SOAP_1_1,
+        "cache_wsdl" => WSDL_CACHE_BOTH,
+        "connection_timeout" => 15,
+        "trace" => false,
+        "encoding" => "UTF-8",
+        "exceptions" => false,
+    );
+
+    $cliente = new SoapClient($wsdl, $options);
+
+    // Establecemos los parámetros de envío
+    if (!empty($_POST['id'])) {
+        // Si los parámetros son correctos, llamamos a la función letra de calcularLetra.php
+        $result = $cliente->FindPerson($params);
+    } else {
+        $error = "<strong>Error:</strong> Debes introducir un DNI correcto<br/><br/>Ej: 45678987";
+    }
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Calcular Letra DNI - Servicio Web + PHP + SOAP</title>
+    <link rel="stylesheet" type="text/css" href="/estilo.css">
+</head>
+
+<body>
+    <h1>Buscar datos de la persona</h1>
+    <h2>Servicio Web + PHP + SOAP</h2>
+    <form action="FindPerson.php" method="post">
+        <?php
+        print "<input type='text' name='id'>";
+        print "<input type='submit' name='enviar' value='Buscar'>";
+        print "<p class='error'>$error</p>";
+        print "<p style='font-size: 12pt;font-weight: bold;color: #0066CC;'>";
+        
+        if (isset($_POST['enviar'])) {
+            foreach ($result as $p) {
+                echo $p->Name . "<br>";
+                echo $p->SSN . "<br>";
+                echo $p->DOB . "<br>";
+
+                $home = $p->Home; 
+                echo "Home:";
+                echo $home->Street . "<br>";
+                echo $home->City . "<br>";
+                echo $home->State . "<br>";
+                echo $home->Zip . "<br>";
+
+                $office = $p->Office;
+                echo "Office:";
+                echo $office->Street . "<br>";
+                echo $office->City . "<br>";
+                echo $office->State . "<br>";
+                echo $office->Zip . "<br>";
+
+                echo "Color:";
+                echo $p->FavoriteColors->FavoriteColorsItem . "<br>";
+            }
+        }
+
+
+        echo "</p>";
+        ?>
+    </form>
+   
+</body>
+
+</html>
